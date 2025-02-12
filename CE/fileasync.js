71bcc54c8aa660b24d4b2c513cf452ee3ea34d22
Abhi <abhi.yadavh2o@gmail.com>
2025-02-12 09:21:45 +0530

    second commit

--- CE/fileasync.js
+++ CE/fileasync.js
@@ -1,1 +1,13 @@
-const fs = required("fs");
+const fs = require("fs/promises");
+const read = async () => {
+    const data = await fs.readFile("./data.txt", "utf-8");
+    console.log(data);
+}
+const write = async () => {
+    const newdata = "This is my new work"
+    await fs.writeFile("./data.txt", newdata,"utf-8")
+}
+
+read();
+
+write();
